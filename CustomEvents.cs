bef1575511300f7d2f360fd4024544010b92e7db
Lydiagyenes <145264775+Lydiagyenes@users.noreply.github.com>
2026-05-19 14:28:52 +0200

    Delete CustomEvents.cs

--- CustomEvents.cs
+++
@@ -1,6 +0,0 @@
-using GDS.Core;
-using GDS.Core.Events;
-
-
-    // A saját eseményünk, ami öröklődik a CustomEvent-ből
-    public record ItemPickedUp(GDS.Core.ItemBase ItemBase, int Quantity) : CustomEvent;
