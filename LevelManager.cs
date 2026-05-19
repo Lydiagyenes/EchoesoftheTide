e5133fca2b8de50448d013e324ac7003c9557a1c
Lydiagyenes <145264775+Lydiagyenes@users.noreply.github.com>
2026-05-19 14:37:29 +0200

    Delete LevelManager.cs

--- LevelManager.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class LevelManager : MonoBehaviour
-{
-    // Jelenleg üres, mert a Pause Menüt már a GameManager kezeli központilag.
-    // Ide később írhatsz pálya-specifikus logikát, ha szükséges.
-}
