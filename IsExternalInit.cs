2467b87cb034ce1f448c4944cc14ade22a6b961e
Lydiagyenes <145264775+Lydiagyenes@users.noreply.github.com>
2026-05-19 14:36:18 +0200

    Delete IsExternalInit.cs

--- IsExternalInit.cs
+++
@@ -1,9 +0,0 @@
-// Ezt a kódrészletet azért kell hozzáadni, hogy a régebbi .NET verziók
-// is megértsék a C# 9.0 'record' és 'init' kulcsszavait.
-// Ez egy standard megoldás a Unity-ban.
-
-
-namespace System.Runtime.CompilerServices
-{
-    internal static class IsExternalInit {}
-}
